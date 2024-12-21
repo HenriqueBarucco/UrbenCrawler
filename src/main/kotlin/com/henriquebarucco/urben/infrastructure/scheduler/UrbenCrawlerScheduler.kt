72195192ae09aa3ddf529fff8e94113d8c9fb2a8
Henrique Barucco <henrique_barucco@hotmail.com>
2024-12-20 21:40:25 -0300

    feat(infrastructure): add logger to scheduler

--- src/main/kotlin/com/henriquebarucco/urben/infrastructure/scheduler/UrbenCrawlerScheduler.kt
+++ src/main/kotlin/com/henriquebarucco/urben/infrastructure/scheduler/UrbenCrawlerScheduler.kt
@@ -3,6 +3,7 @@ package com.henriquebarucco.urben.infrastructure.scheduler
 import com.henriquebarucco.urben.application.urben.VerifyUrbenNewPicturesUseCase
 import com.henriquebarucco.urben.application.urben.VerifyUrbenStatusUseCase
 import com.henriquebarucco.urben.domain.token.GenerateAccessTokenGateway
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -12,11 +13,17 @@ class UrbenCrawlerScheduler(
     private val generateAccessTokenGateway: GenerateAccessTokenGateway,
     private val verifyUrbenNewPicturesUseCase: VerifyUrbenNewPicturesUseCase,
 ) {
+    private val log = LoggerFactory.getLogger(UrbenCrawlerScheduler::class.java)
+
     @Scheduled(cron = "\${scheduler.cron}")
     fun execute() {
+        log.info("Starting Urben Crawler")
+
         val token = this.generateAccessTokenGateway.generate()
 
         this.verifyUrbenStatusUseCase.execute(token)
         this.verifyUrbenNewPicturesUseCase.execute(token)
+
+        log.info("Urben Crawler finished")
     }
 }
